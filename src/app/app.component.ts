d0041fd81e162782e73434169e9d186683e49a5c
Joseba Madrigal <mjoseba@gmail,com>
2021-06-04 17:19:58 +0200

    Hola mundo en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'bookstore';
+  title = 'Librería';
+  book = {
+    title: 'Angular 11 en castellano',
+    author: 'Joseba Madrigal',
+    desc: 'Aprende de 0 a experto construyendo un proyecto real que incluye lazy loading, NGRX y PWA.',
+    price: 9
+  };
 }
