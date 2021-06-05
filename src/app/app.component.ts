3255bbc044c4177650d204e9140a752be5c0752c
Joseba Madrigal <mjoseba@gmail,com>
2021-06-05 19:33:29 +0200

    directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Librería';
-  book = {
-    title: 'Angular 11 en castellano',
-    author: 'Joseba Madrigal',
-    desc: 'Aprende de 0 a experto construyendo un proyecto real que incluye lazy loading, NGRX y PWA.',
-    price: 9
-  };
+
+  books = [
+    {
+      title: 'MongoDb en Castellano',
+      price: 6,
+      author: 'Joseba Madrigal',
+      desc: 'Aprende la base de datos no-relacional MongoDb.',
+      image: 'mongodb.png',
+      pdfonly: true
+    },
+    {
+      title: 'Angular 12 en castellano',
+      price: 9,
+      author: 'Joseba Madrigal',
+      desc: 'Aprende de 0 a experto construyendo un proyecto real que incluye lazy loading, NGRX y PWA.',
+      image: 'angular12.png',
+      pdfonly: true
+    },
+    {
+      title: 'DON QUIJOTE DE LA MANCHA',
+      price: 17,
+      author: 'Miguel de Cervantes',
+      desc: 'El ingenioso hidalgo don Quijote de la Mancha narra las aventuras de Alonso Quijano, un hidalgo pobre que de tanto leer novelas de caballería acaba enloqueciendo y creyendo ser un caballero andante, nombrándose a sí mismo como don Quijote de la Mancha.',
+      image: 'donquijote.jpg',
+      pdfonly: false
+    }
+  ];
 }
