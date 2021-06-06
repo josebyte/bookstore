57e4083ab20bf60895eea723f65d673d170e35db
Joseba Madrigal <mjoseba@gmail,com>
2021-06-06 11:00:03 +0200

    Pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './directives/highlight.directive';
+import localeEsES from '@angular/common/locales/es';
+import { CopyRight } from './copyright.pipe';
+registerLocaleData(localeEsES, 'es-ES');
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HighlightDirective
+    HighlightDirective,
+    CopyRight
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'es-ES' } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
