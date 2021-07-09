d3eddab3c74c1cd29ce2493bdff3d60b943dd0b6
Joseba Madrigal <mjoseba@gmail,com>
2021-07-09 02:47:48 +0200

    custom directive

---
+++ src/app/directives/highlight.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HightlightDirective } from './highlight.directive';
+
+describe('HightlightDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HightlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
